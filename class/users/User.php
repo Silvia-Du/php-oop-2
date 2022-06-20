16dea9cf9e3ef2d383eef83470e4499e7d682e99
Silvia-Du <silviadu@hotmail.it>
2022-06-20 23:09:22 +0200

    Impostato funzioni Set provvisorie per dati dinamici utente

--- class/users/User.php
+++ class/users/User.php
@@ -3,23 +3,24 @@
 class User{
   private $name;
   private $surname;
-  private $phoneNum;
-  private $paymentMethods;
+  private $email;
+
+  private $subscription = false;
+  private $password;
   private $cardNumber;
+
   private $cardExpiryDate;
+  private $checkExpiry = false;
+  private $shippingAdress;
+
   private $ordersList =[];
   private $cartTot;
-  private $shippingAdress;
   private $discount = 0;
 
-  public function __construct( $_name, $_surname, $_phoneNum, $_paymentMethods, $_cardNumber, $_ordersList, $_shippingAdress){
+  public function __construct( $_name, $_surname, $_email ){
     $this->name = $_name;
     $this->surname = $_surname;
-    $this->phoneNum = $_phoneNum;
-    $this->paymentMethods = $_paymentMethods;
-    $this->cardNumber = $_cardNumber;
-    $this->ordersList = $_ordersList;
-    $this->shippingAdress = $_shippingAdress;
+    $this->email = $_email;
 
   }
 
@@ -32,16 +33,46 @@ class User{
     $this->surname = $_surname;
   }
 
-  public function setPhoneNum($_phoneNum){
-    $this->phoneNum = $_phoneNum;
+  public function setEmail($_email){
+    $this->email = $_email;
   }
 
-  public function setPaymentMethods($_paymentMethods){
-    $this->paymentMethods = $_paymentMethods;
+  public function setSubscription($_subscription){
+
+    if($this->password != null){
+      $this->subscription = $_subscription;
+    }
+  }
+
+  public function setPword($_password){
+
+    if(strlen($_password) > 8 )
+    $this->password = $_password;
+  }
+
+
+  public function setcardExpiry($_cardExpiryDate){
+
+    // $cardExpiry = date_create($_cardExpiryDate);
+    // $today = date_create(date('Y-m-d'));
+
+    $diff = date_diff( date_create($_cardExpiryDate) ,  date_create(date('Y-m-d')) );
+    $differenza = (int) $diff->format("%R%a");
+
+    if($differenza >0 ){
+      $this->checkExpiry = true;
+    }else{
+      $this->checkExpiry = false;
+    }
   }
 
   public function setCardNumber($_cardNumber){
-    $this->cardNumber = $_cardNumber;
+    if($this->checkExpiry){
+      $this->cardNumber = $_cardNumber;
+    }else{
+      echo 'Carta non valida, selezionare un\'altro metodo di pagamento';
+    }
+
   }
   //
   public function setOrderList($_ordersList){
@@ -61,12 +92,8 @@ class User{
     $this->surname;
   }
 
-  public function getPhoneNum(){
-    $this->phoneNum;
-  }
-
-  public function getPaymentMethods(){
-    $this->paymentMethods;
+  public function getEmail(){
+    $this->email;
   }
 
   public function getCardNumber(){
@@ -81,5 +108,22 @@ class User{
     $this->shippingAdress;
   }
 
+}
+
+
+$date1=date_create("2013-03-15");
+$date2=date_create("2022-06-20");
+$diff=date_diff($date1,$date2);
+// echo $diff->format("%R%a");
+
+$differenza = (int) $diff->format("%R%a");
+// echo (int) $diff->format("%R%a");
+// echo intval($diff->format("%R%a"));
+
+echo $differenza;
+if($differenza > 0){
+  echo 'numero';
+}else{
+  echo 'stringa';
 }
 ?>
